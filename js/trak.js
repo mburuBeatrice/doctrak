fdc3f137f2cf30c5da97ead7ae0b12b932031419
beatrice mburu <mburubeatricewanjiru@gmail.com>
2017-11-09 11:39:07 +0300

    Adds field inputs

--- js/trak.js
+++ js/trak.js
@@ -5,17 +5,15 @@ function loadDoc() {
   if (this.readyState == 4 && this.status == 200) {
     var fer = JSON.parse(this.responseText);
    if (fer.id !== "undefined") {
-    document.getElementById("results").innerHTML = "Name: " +fer.name 
-   + "<br>Date: " + fer.created 
-   + "<br>Found By: " + fer.author_name
-   + "<br>Identity Code: " + fer.id
-   + "<br>Status: " + fer.state
-   + "<br>Other Details: " + fer.details;
+    document.getElementById("submitForm").innerHTML = "<div class=\"black\">Name: " +fer.name
+   + "<br>Email: " + fer.email
+   + "<br>Found By: " + fer.website
+   + "<br>Identity Code: " + fer.id+"</div>";
     } else {
-      document.getElementById("results").innerHTML = "Not found";
+      document.getElementById("submitForm").innerHTML = "Not found";
     }
   }
   };
-  xhttp.open("GET", "https://mauko.co.ke/api/posts/"+unumber, true);
+  xhttp.open("GET", "https://jsonplaceholder.typicode.com/users/"+unumber, true);
   xhttp.send();
 }
