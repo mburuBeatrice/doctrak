68745c05e79db96840e200df3b0d3ba967a7ca2b
beatrice mburu <mburubeatricewanjiru@gmail.com>
2017-11-09 18:46:57 +0300

    adds the API

--- js/trak.js
+++ js/trak.js
@@ -5,10 +5,10 @@ function loadDoc() {
   if (this.readyState == 4 && this.status == 200) {
     var fer = JSON.parse(this.responseText);
    if (fer.id !== "undefined") {
-    document.getElementById("submitForm").innerHTML = "<div class=\"black\">Name: " +fer.name
+    document.getElementById("submitForm").innerHTML = "<div class=\"black paddy\">Name: " +fer.name
    + "<br>Email: " + fer.email
    + "<br>Found By: " + fer.website
-   + "<br>Identity Code: " + fer.id+"</div>";
+   + "<br>Identity Code: " + fer.id+"</div><a href=\"\" class=\"btn btn-unique\">Refresh <i class=\"fa material-icons ml-1\">refresh</i></a>";
     } else {
       document.getElementById("submitForm").innerHTML = "Not found";
     }
